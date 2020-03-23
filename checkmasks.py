1b0e021ddb359a79981b10be2f11fb79a063b08a
Kelley M. Hess <hess@astro.rug.nl>
2020-03-23 10:16:33 +0100

    Fixed bug in profile plotting when np.sum is nan

--- checkmasks.py
+++ checkmasks.py
@@ -76,8 +76,8 @@ for b in beams:
                 hdu_mask = fits.open(loc + cube_name + '{}_4sig_mask-2d.fits'.format(c))
                 mask2d = hdu_mask[0].data[:, :]
 
-                mask2d=np.asfarray(mask2d)
-                mask2d[mask2d<1] = np.nan
+                mask2d = np.asfarray(mask2d)
+                mask2d[mask2d < 1] = np.nan
 
                 cube_frequencies = chan2freq(np.array(range(hdu_filter[0].data.shape[0])), hdu=hdu_filter)
                 optical_velocity = cube_frequencies.to(u.km/u.s, equivalencies=optical_HI)
@@ -87,19 +87,20 @@ for b in beams:
                 ax_im[c-1].set_title("Cube {}".format(c))
                 for s in range(len(cat)):
                     ax_im[c-1].text(cat['col3'][s] + np.random.uniform(-40, 40), cat['col4'][s] + np.random.uniform(-40, 40),
-                             cat['col2'][s], color='black')
+                                    cat['col2'][s], color='black')
                     # print(cat['col2'][s], wcs.pixel_to_world(cat['col3'][s], cat['col4'][s]).to_string('hmsdms'))
-                    spectrum = np.sum(hdu_filter[0].data[:,mask2d==cat['col2'][s]],axis=1)
+                    spectrum = np.nansum(hdu_filter[0].data[:, mask2d == cat['col2'][s]], axis=1)
                     maskmin = chan2freq(cat['col10'][s], hdu=hdu_filter).to(u.km/u.s, equivalencies=optical_HI).value
                     maskmax = chan2freq(cat['col11'][s], hdu=hdu_filter).to(u.km/u.s, equivalencies=optical_HI).value
-                    ax_spec[previous + s, 0].plot([optical_velocity[-1].value,optical_velocity[0].value], [0, 0], '--', color='gray')
+                    ax_spec[previous + s, 0].plot([optical_velocity[-1].value, optical_velocity[0].value], [0, 0], '--', color='gray')
                     ax_spec[previous + s, 0].plot(optical_velocity, spectrum, c=colors[c-1])
                     ax_spec[previous + s, 0].plot([maskmin, maskmin], [np.nanmin(spectrum), np.nanmax(spectrum)], ':', color='gray')
                     ax_spec[previous + s, 0].plot([maskmax, maskmax], [np.nanmin(spectrum), np.nanmax(spectrum)], ':', color='gray')
                     ax_spec[previous + s, 0].set_title("Beam {:02}, Cube {}, Source {}".format(b, c, cat['col2'][s]))
-                    ax_spec[previous + s, 0].set_xlim(optical_velocity[-1].value,optical_velocity[0].value)
+                    ax_spec[previous + s, 0].set_xlim(optical_velocity[-1].value, optical_velocity[0].value)
                     ax_spec[previous + s, 0].set_ylabel("Integrated Flux")
-                    if (previous + s == source_per_beam - 1): ax_spec[previous + s, 0].set_xlabel("Optical Velocity [km/s]")
+                    if previous + s == source_per_beam - 1:
+                        ax_spec[previous + s, 0].set_xlabel("Optical Velocity [km/s]")
                 previous += len(cat)
 
                 hdu_mask.close()
