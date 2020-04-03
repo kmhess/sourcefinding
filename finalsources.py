905360e5565dd16d3f1a3960df0cebb6fbdeafef
Kelley M. Hess <hess@astro.rug.nl>
2020-04-03 14:14:55 +0200

    Huge finalsources.py revamp. Use SoFiA cubelets.py moments.

--- finalsources.py
+++ finalsources.py
@@ -7,8 +7,7 @@ from astropy import units as u
 import numpy as np
 
 sys.path.insert(0, os.environ['SOFIA_MODULE_PATH'])
-from sofia import writemoment2
-
+from sofia import cubelets
 
 def chan2freq(channels=None, hdu=None):
     frequencies = (channels * hdu[0].header['CDELT3'] + hdu[0].header['CRVAL3']) * u.Hz
@@ -41,46 +40,58 @@ if '-' in args.beams:
 else:
     beams = [int(b) for b in args.beams.split(',')]
 
-HI_restfreq = 1420405751.77 * u.Hz
-optical_HI = u.doppler_optical(HI_restfreq)
-
 cube_name = 'HI_image_cube'
 for b in beams:
     loc = '/tank/hess/apertif/' + taskid + '/B0' + str(b).zfill(2) + '/'
+    catalog = ascii.read(loc + 'clean_cat.txt')
 
     for c in cubes:
         if os.path.isfile(loc + cube_name + '{}_clean.fits'.format(c)):
-            # Make moment maps:
-            cat = ascii.read(loc + cube_name + '{}_4sig_cat.txt'.format(c))
+            cat = catalog[catalog['cube'] == c]
+            cathead = np.array(cat.colnames)
             print("Found {} sources in Beam {:02} Cube {}".format(len(cat), b, c))
+
+            # Make moment maps:
             hdu_clean = fits.open(loc + cube_name + '{}_clean.fits'.format(c))
             hdu_mask3d = fits.open(loc + cube_name + '{}_4sig_mask.fits'.format(c))
-            writemoment2.writeMoments(hdu_clean[0].data, hdu_mask3d[0].data, loc+cube_name+'{}_clean'.format(c),
-                                      False, hdu_clean[0].header, False, True, True, True)
+            outname = 'src_taskid{}_beam{:02}_cube{}'.format(taskid, b, c)
 
+            # Make cubelets around each individual source, mom0,1,2 maps, and sub-spectra
+            objects = []
+            for source in cat:
+                obj = []
+                for s in source:
+                    obj.append(s)
+                objects.append(obj)
+            objects = np.array(objects)
+            cubelets.writeSubcube(hdu_clean[0].data, hdu_clean[0].header, hdu_mask3d[0].data, objects, cathead,
+                                  outname, loc, False, False)
+
+            # Get beam size and cell size
             bmaj = hdu_clean[0].header['BMAJ'] * 3600. * u.arcsec
             bmin = hdu_clean[0].header['BMIN'] * 3600. * u.arcsec
             cellsize = hdu_clean[0].header['CDELT2'] * 3600. * u.arcsec
 
-            # Make HI profiles:  **** NEED TO IMPROVE THIS BY STARTING WITH 3D MASK?!! *****
-            hdu_mask2d = fits.open(loc + cube_name + '{}_4sig_mask-2d.fits'.format(c))
-            mask2d = hdu_mask2d[0].data[:, :]
-
-            mask2d = np.asfarray(mask2d)
-            mask2d[mask2d < 1] = np.nan
-
+            # Make individual HI profiles over whole cube by squashing 3D mask:
             cube_frequencies = chan2freq(np.array(range(hdu_clean[0].data.shape[0])), hdu=hdu_clean)
-
-            for s in range(len(cat)):
-                spectrum = np.sum(hdu_clean[0].data[:, mask2d == cat['col2'][s]], axis=1)
-                ascii.write([cube_frequencies, spectrum], loc + 'HI_image_cube{}_clean_source{}.txt'.format(c, s),
+            for obj in objects:
+                mask_one = np.zeros(hdu_mask3d[0].data.shape)
+                mask_one[hdu_mask3d[0].data == obj[0]] = 1
+                # Can potentially save this as a better nchan if need be:
+                mask2d = np.sum(mask_one, axis=0)
+                spectrum = np.nansum(hdu_clean[0].data[:, mask2d != 0], axis=1)
+                ascii.write([cube_frequencies, spectrum], loc + outname + '_{}_specfull.txt'.format(int(obj[0])),
                             names=['Frequency [Hz]', 'Flux [Jy/beam*pixel]'])
                 os.system('echo "# BMAJ = {}\n# BMIN = {}\n# CELLSIZE = {:.2f}" > temp'.format(bmaj, bmin, cellsize))
-                os.system('cat temp ' + loc + 'HI_image_cube{}_clean_source{}.txt'.format(c, s) +
-                          ' > temp2 && mv temp2 ' + loc + 'HI_image_cube{}_clean_source{}.txt'.format(c, s))
+                os.system('cat temp ' + loc + outname + '_{}_specfull.txt'.format(int(obj[0])) + ' > temp2 && mv temp2 '
+                          + loc + outname + '_{}_specfull.txt'.format(int(obj[0])))
                 os.system('rm temp')
+
+            hdu_clean.close()
+            hdu_mask3d.close()
 
         else:
             print("No CLEAN cube for Beam {:02}, Cube {}".format(b, c))
 
-print("Beam information can be read from the text files like: a.meta['comments'][0].replace('= ','').split()")
+print("Beam information in *specfull.txt can be read from the text files like: a.meta['comments'][0].replace('= ','').split()")
+print("[FINALSOURCES] Done.")
