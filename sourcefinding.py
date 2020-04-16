4f291564150ff33114ff22106a340fc7af78a67f
Kelley M. Hess <hess@astro.rug.nl>
2020-04-16 14:15:40 +0200

    Removd a bunch of unused sofia calls & commented lines

--- sourcefinding.py
+++ sourcefinding.py
@@ -1,7 +1,6 @@
 import os
 
 from argparse import ArgumentParser, RawTextHelpFormatter
-from astropy.io import fits
 import numpy as np
 
 
@@ -44,12 +43,10 @@ parser.add_argument('-o', "--overwrite",
                     help="If option is included, overwrite old continuum filtered file if it exists.",
                     action='store_true')
 
-
 # Parse the arguments above
 args = parser.parse_args()
 
 # Range of cubes/beams to work on:
-# dynamic_range_limit = 15.
 taskid = args.taskid
 cubes = [int(c) for c in args.cubes.split(',')]
 if '-' in args.beams:
@@ -76,26 +73,11 @@ for b in beams:
         # Check to see if the continuum filtered file exists.  If not, make it  with SoFiA-2
         if (not overwrite) & os.path.isfile(filteredfits):
             print("[SOURCEFINDING] Continuum filtered file exists and will not be overwritten.")
-            # with fits.open(filteredfits, mode='update') as f:
-            #     # mask = np.ones(f[0].data.shape[0], dtype=bool)  # Commented out because doesn't seem to affect DR when using filtered cube.
-            #     # if c == 3: mask[376:662] = False
-            #     nanmax, nanstd = np.nanmax(f[0].data), np.nanstd(f[0].data)
-            #     dynamic_range = nanmax / nanstd
-            #     print("\tPsuedo dynamic range, max, std are: {}, {}, {}".format(dynamic_range, nanmax, nanstd))
         elif os.path.isfile(sourcefits):
             print("[SOURCEFINDING] Making continuum filtered file.")
             os.system('grep -vwE "(input.data)" template_filtering.par > ' + loc + 'filtering.par')
-            # if c == 3:
-            #     os.system('grep -vwE "(flag.region)" ' + loc + 'filtering.par > temp && mv temp ' + loc + 'filtering.par')
-            #     os.system('echo "flag.region                =  0,661,0,661,375,601" >> ' + loc + 'filtering.par')
             os.system('echo "input.data                 =  ' + sourcefits + '" >> ' + loc + 'filtering.par')
             os.system('/home/apercal/SoFiA-2/sofia ' + loc + 'filtering.par >> test.log')
-            # with fits.open(filteredfits) as f:
-            #     # mask = np.ones(f[0].data.shape[0], dtype=bool)  # Commented out because doesn't seem to affect DR
-            #     # if c == 3: mask[376:662] = False
-            #     nanmax, nanstd = np.nanmax(f[0].data), np.nanstd(f[0].data)
-            #     dynamic_range = nanmax / nanstd
-            #     print("\tPsuedo dynamic range, max, std are: {}, {}, {}".format(dynamic_range, nanmax, nanstd))
         else:
             print("\tBeam {:02} Cube {} is not present in this directory.".format(b, c))
             continue
@@ -104,21 +86,3 @@ for b in beams:
         sig = 4
         new_paramfile, outlog = make_param_file(sig=sig, loc_dir=loc, cube_name=cube_name, cube=c)
         os.system('/home/apercal/SoFiA-2/sofia ' + new_paramfile + ' >> ' + outlog)
-
-        # After filtering, if the DR (presumably from a bright HI source) is > 14 Do an initial source finding at sn=10.
-        # if (c != 1) & (dynamic_range >= dynamic_range_limit):
-        #     print("\tNEED TO DO HIGH THRESHOLD (10 sigma) SOURCE FINDING FIRST.")
-        #     sig = 10
-        #     new_paramfile, outlog = make_param_file(sig=sig, loc_dir=loc, cube_name=cube_name, cube=c)
-        #     os.system('/home/apercal/SoFiA-2/sofia ' + new_paramfile + ' >> ' + outlog)
-        # else:
-        #     print("\tDOING 4 sigma SOURCE FINDING.")
-        #     sig = 4
-        #     new_paramfile, outlog = make_param_file(sig=sig, loc_dir=loc, cube_name=cube_name, cube=c)
-        #     os.system('/home/apercal/SoFiA-2/sofia ' + new_paramfile + ' >> ' + outlog)
-        #
-        # if (sig == 10) & (not os.path.isfile(loc + cube_name + '10sig_cat.txt')):
-        #     print("\t10 sigma found nothing. DOING 4 sigma SOURCE FINDING.")
-        #     sig = 4
-        #     new_paramfile, outlog = make_param_file(sig=sig, loc_dir=loc, cube_name=cube_name, cube=c)
-        #     os.system('/home/apercal/SoFiA-2/sofia ' + new_paramfile + ' >> ' + outlog)
